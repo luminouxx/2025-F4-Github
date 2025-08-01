a2f029e5eec5779256f70c3a91fffe298228fbe3
Donghyun An <ehdgus8725@naver.com>
2025-08-01 17:15:36 +0900

    [Feat] 목련 뷰 제작

---
+++ 2025-F4-Github/2025-F4-Github/View/LearnerViews/MokryunView.swift
@@ -0,0 +1,28 @@
+//
+//  MokryunView.swift
+//  2025-F4-Github
+//
+//  Created by 목련 on 8/1/25.
+//
+
+import SwiftUI
+
+struct MokryunView: LearnerView {
+    // TODO: 1. 이름, 팀 수정하고 커밋하기
+    var name: String = "Mokryun"
+    
+    var team: String = "사과밥"
+    
+    var body: some View {
+        Text("저의 이름은 \(name)입니다.")
+            .font(.largeTitle)
+        Text("저의 팀은 \(team) 입니다.")
+            .font(.subheadline)
+    }
+}
+
+#Preview {
+    MokryunView()
+}
+
+
